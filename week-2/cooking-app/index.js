9f6d0999da1bb56c44116c30dcb91c687ae3a098
John <john.kuronya@outlook.com>
2025-03-30 09:24:45 -0400

    Third Commit

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,60 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: John Kuronya
+ * Date: 3/24/25
+ * File Name: index.js
+ * Description: CLI program demonstrating recipe functions.
+ */
 
-// TODO: Import your module using require
+//Module using require
+const readline = require("readline");
+const { createRecipe, setTimer, quit } = require("./recipes");
 
-// TODO: Implement your CLI program here
+// Readline interface for CLI interaction
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Function to display menu
+function showMenu() {
+  console.log("\nRecipe CLI Application");
+  console.log("1. Create Recipe");
+  console.log("2. Set Timer");
+  console.log("3. Quit");
+  rl.question("Choose an option (1-3): ", handleUserInput);
+}
+
+// Handle user input
+function handleUserInput(choice) {
+  switch (choice) {
+    case "1":
+      rl.question("Enter ingredients (comma separated): ", (input) => {
+        const ingredients = input.split(",").map(ing => ing.trim());
+        console.log(createRecipe(ingredients));
+        showMenu();
+      });
+      break;
+    case "2":
+      rl.question("Enter time in minutes: ", (input) => {
+        const minutes = parseInt(input, 10);
+        if (isNaN(minutes) || minutes <= 0) {
+          console.log("Please enter a valid number of minutes.");
+        } else {
+          console.log(setTimer(minutes));
+        }
+        showMenu();
+      });
+      break;
+    case "3":
+      console.log(quit());
+      rl.close();
+      break;
+    default:
+      console.log("Invalid choice. Please enter 1, 2, or 3.");
+      showMenu();
+  }
+}
+
+// Start the CLI
+showMenu();
+
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,28 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: John Kuronya
+ * Date: 3/24/25
+ * File Name: recipes.js
+ * Description: Recipe related functions for the Node.js CLI app.
+ */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  return `Recipe created with ingredients: ${ingredients.join(", ")}`;
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
-  // TODO: Implement this function
+  return `Timer set for ${minutes} minutes`;
 }
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+  return "Program exited";
 }
 
-// TODO: Export the functions
+// Export the functions
+module.exports = {
+  createRecipe,
+  setTimer,
+  quit
+};
