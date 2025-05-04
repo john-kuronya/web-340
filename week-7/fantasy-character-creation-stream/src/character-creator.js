7f35199cb23dad7819bb156ad13c11684bd197e8
John <john.kuronya@outlook.com>
2025-05-04 19:47:14 -0400

    Seventh Commit

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -2,16 +2,38 @@ const { Duplex } = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
-    super(options);
-    // TODO: Initialize your class here
+    super({ ...options, readableObjectMode: true, writableObjectMode: true });
+    this.characterData = null;
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    try {
+      if (!chunk || typeof chunk !== 'object') {
+        return callback(new Error('Invalid character data.'));
+      }
+
+      const { class: charClass, gender, funFact } = chunk;
+
+      if (!charClass || !gender || !funFact) {
+        return callback(new Error('Missing required fields.'));
+      }
+
+      this.characterData = { charClass, gender, funFact };
+      callback(); // success
+    } catch (err) {
+      callback(err);
+    }
   }
 
   _read(size) {
-    // TODO: Implement your _read method here
+    if (this.characterData) {
+      const { charClass, gender, funFact } = this.characterData;
+      const output = `You have created a ${gender} ${charClass}. Fun fact: ${funFact}.`;
+      this.push(output);
+      this.push(null); // end of stream
+    } else {
+      this.push(null);
+    }
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -7,15 +7,47 @@ describe('CharacterCreator', () => {
     characterCreator = new CharacterCreator();
   });
 
-  test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+  test('should process data correctly when written to', (done) => {
+    const input = {
+      class: 'Warrior',
+      gender: 'Male',
+      funFact: 'Loves dragons'
+    };
+
+    characterCreator.write(input, () => {
+      characterCreator.on('data', (chunk) => {
+        expect(chunk).toBe('You have created a Male Warrior. Fun fact: Loves dragons.');
+        done();
+      });
+
+      characterCreator.read();
+    });
   });
 
-  test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+  test('should emit "error" when invalid data is written', (done) => {
+    characterCreator.on('error', (err) => {
+      expect(err).toBeInstanceOf(Error);
+      expect(err.message).toBe('Invalid character data.');
+      done();
+    });
+
+    characterCreator.write('', () => {}); // writing an empty string (invalid)
   });
 
-  test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+  test('should transform data correctly when written to', (done) => {
+    const input = {
+      class: 'Mage',
+      gender: 'Female',
+      funFact: 'Can summon lightning'
+    };
+
+    characterCreator.write(input, () => {
+      characterCreator.on('data', (chunk) => {
+        expect(chunk).toBe('You have created a Female Mage. Fun fact: Can summon lightning.');
+        done();
+      });
+
+      characterCreator.read();
+    });
   });
 });
